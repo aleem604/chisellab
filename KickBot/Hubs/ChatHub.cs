da98f34e720c34c674c5ddf4778b96aeea6a14e2
M Aleem <aleem604@gmail.com>
2020-03-10 22:42:23 +0500

    initial commit

---
+++ KickBot/Hubs/ChatHub.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KickBot.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("UserConnected", Context.ConnectionId);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            await Clients.All.SendAsync("UserDisconnected", Context.ConnectionId);
+            await base.OnDisconnectedAsync(ex);
+        }
+        public async Task SendMessage(string username, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", username, message);
+        }
+        public async Task SendToAll(string connectionId, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", connectionId, message);
+        }
+        public Task SendToOthers(string message)
+        {
+            return Clients.Others.SendAsync("Send", $"{Context.ConnectionId}: {message}");
+        }
+
+        public Task SendToConnection(string connectionId, string message)
+        {
+            return Clients.Client(connectionId).SendAsync("Send", $"Private message from {Context.ConnectionId}: {message}");
+        }
+
+        public Task SendToGroup(string groupName, string message)
+        {
+            return Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId}@{groupName}: {message}");
+        }
+
+        public Task SendToOthersInGroup(string groupName, string message)
+        {
+            return Clients.OthersInGroup(groupName).SendAsync("Send", $"{Context.ConnectionId}@{groupName}: {message}");
+        }
+
+        public async Task JoinGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} joined {groupName}");
+        }
+
+        public async Task LeaveGroup(string groupName)
+        {
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} left {groupName}");
+
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+        }
+
+        public Task Echo(string message)
+        {
+            return Clients.Caller.SendAsync("Send", $"{Context.ConnectionId}: {message}");
+        }
+
+    }
+}
---
+++ KickBot/wwwroot/js/chat.js
@@ -0,0 +1,104 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+var input = document.getElementById("messageInput");
+input.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("sendButton").click();
+    }
+});
+
+connection.on("ReceiveMessage", function (user, message) {
+
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var elements = createmyElements(user, msg);
+    document.getElementById("messagesList").appendChild(elements);
+    document.getElementById("messagesListAdmin").appendChild(elements);
+});
+
+connection.on("UserConnected", function (connectionId) {
+    var elements = addInChatList(connectionId);
+    document.getElementById("chatList").appendChild(elements);    
+});
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    debugger;
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+function createmyElements(user, message) {
+
+    if (user !== 'admin') {
+        var d1 = document.createElement("div");
+        d1.setAttribute("class", "row msg_container base_sent");
+        var d2a = document.createElement("div");
+        d2a.setAttribute("class", "col-md-10 col-xs-10");
+        var d2b = document.createElement("div");
+        d2b.setAttribute("class", "col-md-2 col-xs-2 avatar");
+        var d3 = document.createElement("div");
+        d3.setAttribute("class", "messages msg_sent");
+        var p = document.createElement("p");
+        p.textContent = message;
+        var img = document.createElement("img");
+        img.setAttribute("src", "http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg");
+        img.setAttribute("class", "img-responsive");
+        d1.appendChild(d2a).appendChild(d3).appendChild(p);
+        d1.appendChild(d2b).appendChild(img);
+    }
+    else if (user === 'admin') {
+        var d1 = document.createElement("div");
+        d1.setAttribute("class", "row msg_container base_receive");
+        var d2a = document.createElement("div");
+        d2a.setAttribute("class", "col-md-10 col-xs-10");
+        var d2b = document.createElement("div");
+        d2b.setAttribute("class", "col-md-2 col-xs-2 avatar");
+        var d3 = document.createElement("div");
+        d3.setAttribute("class", "messages msg_receive");
+        var p = document.createElement("p");
+        p.textContent = message;
+        var img = document.createElement("img");
+        img.setAttribute("src", "http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg");
+        img.setAttribute("class", "img-responsive");
+        d1.appendChild(d2b).appendChild(img);
+        d1.appendChild(d2a).appendChild(d3).appendChild(p);
+
+    }
+
+    return d1;
+}
+
+function addInChatList(connectionId) {
+    var d1 = document.createElement("div");
+    d1.setAttribute("class", "chat_list active_chat");
+    var d2 = document.createElement("div");
+    d2.setAttribute("class", "chat_people");
+    var d2a = document.createElement("div");
+    d2a.setAttribute("class", "chat_img");
+    var d2b = document.createElement("div");
+    d2b.setAttribute("class", "chat_ib");
+    var h = document.createElement("h5");
+    var span = document.createElement("span");
+    span.setAttribute("class","chat_date")
+    var p = document.createElement("p");
+    p.textContent = connectionId;
+    var img = document.createElement("img");
+    img.setAttribute("src", "http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg");
+   
+    d1.appendChild(d2).appendChild(d2a).appendChild(img);
+    d2.appendChild(d2b).appendChild(h).appendChild(span).appendChild(p);
+    return d1;
+}
